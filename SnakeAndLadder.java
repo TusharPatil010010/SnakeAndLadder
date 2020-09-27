672a9862c815c3a972c9ce8c7aca138ede6f401f
TusharPatil010010 <tusharp010010@gmail.com>
2020-09-27 23:29:34 +0530

    UC2 committed

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,9 @@
 public class SnakeAndLadder{
-public static void main(String[] args) {
-int pos=0;
-		System.out.println("Players start position = 0");
+	public static void main(String[] args) {
+		int start = 0;
+		int check = (int)Math.floor(Math.random()*10)%6;
+		
+		System.out.println("Player1 starts at " + start);
+		System.out.println("Player1 starts by rolling " + check);
 	}
 }
